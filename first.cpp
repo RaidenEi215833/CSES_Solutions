10f812598b57701d12fb230ab7573edfdafc921e
RaidenEi215833 <satoh215833@gmail.com>
2025-07-07 10:44:37 +0700

    lmao

---
+++ first.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define fi first
+#define se second
+#define pll pair<ll,ll>
+
+using namespace std;
+
+void input()
+{
+    
+}
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    #define orz "mo"
+    if (fopen(orz ".inp", "r")) {
+        freopen(orz ".inp", "r", stdin);
+        freopen(orz ".out", "w", stdout);
+    }
+
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        input();
+        solve();
+    }
+
+    cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s";
+    return 0;
+}
